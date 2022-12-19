f9c7a78ae50a90c53ee21bafed1ca08bb467b8d7
Solomon Nsumei <solnsumei@gmail.com>
2022-12-19 09:45:32 +0100

    add Entry struct, search and list functions and initial main implementations

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"path"
+)
 
+type Entry struct {
+	Name    string
+	Surname string
+	Tel     string
+}
+
+var data = []Entry{}
+
+func search(key string) *Entry {
+	for i, v := range data {
+		if v.Surname == key {
+			return &data[i]
+		}
+	}
+	return nil
+}
+
+func list() {
+	for _, v := range data {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		exe := path.Base(arguments[0])
+		fmt.Printf("Usage: %s search|list <arguments>\n", exe)
+		return
+	}
 }
